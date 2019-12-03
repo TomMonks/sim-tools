57e8cfb7a2991586f1e8c7950c2f26448f874cf3
Tom Monks <thomas.m.monks@gmail.com>
2019-12-03 16:35:37 +0000

    started KN++

---
+++ ovs/fixed_budget.py
@@ -0,0 +1,10 @@
+'''
+Contains algorithms for Optimisation via Simulation
+with a fixed budget (fixed total number of replications)
+
+1. OCBA - optimal computing budget allocation
+2. OCBA-m - optimal computering budget allocation top m.
+3. KNPlusPlus - KN++ sequential Ranking and Selection Algorithm
+
+'''
+
