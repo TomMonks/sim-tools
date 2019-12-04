a7004129b3dd7d1f373398de6a930045fc35b347
Tom Monks <thomas.m.monks@gmail.com>
2019-12-04 16:24:31 +0000

    converted to KN

--- ovs/toy_models.py
+++ ovs/toy_models.py
@@ -77,7 +77,7 @@ class GaussianBandit(object):
         reward -- int with value = 0 when no reward
                   and 1 when the pull results in a win
         '''      
-        reward = np.random.normal(self._mu, self._sigma, size=replications)
+        reward = np.random.normal(self._mu, self._sigma)
 
         self._total_reward += reward
         self._number_of_plays += 1
@@ -96,13 +96,72 @@ class GaussianBandit(object):
 class StandardRankingSelectionProblem(object):
     def __init__(self, designs, random_state=None):
         self._designs = designs
-        self._bandits = np.array(guassian_bandit_sequence(1, designs))
-
+        self._env = BanditCasino(guassian_bandit_sequence(1, designs))
+        
         if random_state is not None:
             np.random.seed(random_state)
 
     def simulate(designs, replications):
         for bandit in self._bandits[designs]:
-            bandit.play(repliations)
+            bandit.play(replications)
 
 
+class BanditCasino(object):
+    
+    def __init__(self, bandits):
+        '''
+        Casino constructor method
+
+        Keyword arguments:
+        ------
+        bandits -- list, of BernoulliBandits objects
+        '''
+        self._bandits = bandits
+        self._current_index = 0
+        self._observers = []
+    
+    def __getitem__(self, index):
+        return self._bandits[index]
+
+    def __get_number_of_arms(self):
+        return len(self._bandits)
+    
+    def action(self, bandit_index):
+        '''
+        Play a specific bandit machine.
+
+        Notifies all observers of the outcome 
+
+        Keyword arguments:
+        -----
+        bandit_index -- int, index of bandit to play 
+        '''
+        reward = self._bandits[bandit_index].play()
+        self.notify_observers(bandit_index, reward)
+
+    def random_action(self):
+        '''
+        Selects a bandit index at random and plays it.
+        '''
+        bandit_index = np.random.choice(len(self._bandits))
+        self.action(bandit_index)
+
+       
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self._current_index += 1
+        if self._current_index > len(self._bandits):
+            raise StopIteration
+        else:
+            return self._bandits[self._current_index - 1]
+
+    def register_observer(self, observer):
+        self._observers.append(observer)
+ 
+    def notify_observers(self, *args, **kwargs):
+        for observer in self._observers:
+            observer.feedback(self, *args, **kwargs) 
+
+    number_of_arms = property(__get_number_of_arms)
